e03671fe0d6e900435bdd9e598fe6359ddb87ae0
donghyun-Kim0405 <dhkim0405@naver.com>
2021-12-23 00:28:43 +0900

    create project

---
+++ src/main/java/com/example/AndroidAPIServer/AndroidApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.AndroidAPIServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndroidApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AndroidAPIServer/AndroidApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AndroidAPIServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
