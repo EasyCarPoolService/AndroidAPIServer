7853fc5c5d868997e07f92f4af232b180c2c5f44
DHKim <dhkim0405@naver.com>
2022-02-08 18:31:09 +0900

    추가 : ImageController - android profile이미지 로드기능 추가

---
+++ src/main/java/com/example/AndroidAPIServer/controller/ImageController.java
@@ -0,0 +1,42 @@
+package com.example.AndroidAPIServer.controller;
+
+
+import com.example.AndroidAPIServer.dto.user.AndroidLocalUserDto;
+import com.example.AndroidAPIServer.dto.user.LoginDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/image")
+public class ImageController {
+
+
+    @GetMapping("/profile")
+    public ResponseEntity<FileSystemResource> getProfileImage(@RequestParam String email) throws IOException {
+
+        String savePath = System.getProperty("user.dir") + "/profile/"+email+"_profile.jpg";
+
+        Path path = Paths.get(savePath);
+
+        String contentType = Files.probeContentType(path);
+
+        FileSystemResource resource = new FileSystemResource(savePath);
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", contentType);
+
+        return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+    }
+}
