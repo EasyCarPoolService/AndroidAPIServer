bf202ae2b0cb0f151cb866dca89fdd2e02da59ca
DHKim <dhkim0405@naver.com>
2022-02-08 16:23:34 +0900

    수정 : 회원가입시 프로필 이미지 전송받을 수 있도록 수정(이미지 손상 여부 체크)

--- src/main/java/com/example/AndroidAPIServer/controller/UserController.java
+++ src/main/java/com/example/AndroidAPIServer/controller/UserController.java
@@ -7,10 +7,12 @@ import com.example.AndroidAPIServer.jwt.TokenProvider;
 import com.example.AndroidAPIServer.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.Part;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
@@ -23,13 +25,74 @@ public class UserController {
 
     private final TokenProvider tokenProvider;
 
+
+    /*@Part profile_image : MultipartBody.Part?,
+    @Part("name") name: RequestBody?,
+    @Part("email") email: RequestBody?,
+    @Part("nickname") nickname: RequestBody?,
+    @Part("password") password: RequestBody?,
+    @Part("birth") birth: RequestBody?,
+    @Part("gender") gender: RequestBody?,
+    @Part("driverAuthentication") driverAuthentication: RequestBody?,*/
+
+
+    //순서 회원가입 진행완료 후 이어서 사진 저장하도록 설정
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody JoinDto joinDto){
+    public ResponseEntity<String> signup(
+            @RequestParam("profile_image") MultipartFile profile_image,
+            @RequestParam("name") String name,
+            @RequestParam("email") String email,
+            @RequestParam("nickname") String nickname,
+            @RequestParam("password") String password,
+            @RequestParam("birth") String birth,
+            @RequestParam("gender") String gender
+    ){
 
         String responseMessage;
 
+        //join Dto구성 코드 작성
+        JoinDto joinDto = JoinDto.builder()
+                .name(name)
+                .email(email)
+                .nickname(nickname)
+                .password(password)
+                .birth(birth)
+                .gender(gender)
+                .driverAuthentication(false)
+                .build();
+
         try{
             responseMessage = userService.signup(joinDto);
+
+            //signup 수행후 이미지 저장 수행
+            try {
+                String origFilename = profile_image.getOriginalFilename();
+                String filename = origFilename.toString();
+
+                //savePath -> 현재 프로젝트 폴더내 존재하는 files폴더의 경로
+                String savePath = System.getProperty("user.dir") + "/profile";
+
+                // /files폴더가 존재하지 않으면 생성
+                if (!new File(savePath).exists()) {
+                    try{
+                        new File(savePath).mkdir();
+                    }
+                    catch(Exception e){
+                        e.getStackTrace();
+                    }
+                }
+
+                //   현재프로젝트경로/files/filename   ->  filename == (androdi)fileName 변수
+                String filePath = savePath + "/" + filename;
+
+                // 파일 저장
+                profile_image.transferTo(new File(filePath));
+
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+
+
         }catch(Exception e){
             responseMessage = e.getMessage();
         }
@@ -55,12 +118,19 @@ public class UserController {
     //check
     @PreAuthorize("hasAnyRole('USER')")
     @PostMapping("/driverAuth")
-    public ResponseEntity<String> driverAuthentication(@RequestParam("idImage")MultipartFile id_image, @RequestParam("carImage")MultipartFile car_image) {
+    public ResponseEntity<String> driverAuthentication(@RequestParam("idImage")MultipartFile id_image,
+                                                       @RequestParam("carImage")MultipartFile car_image,
+                                                       @RequestParam("testData")Part testData) {
 
         String responseMessage = "ok";
 
+
         System.out.println(id_image.getOriginalFilename());
         System.out.println(car_image.getOriginalFilename());
+
+        System.out.println(testData.name());
+
+
 
         /*try {
             String origFilename = files.getOriginalFilename();
